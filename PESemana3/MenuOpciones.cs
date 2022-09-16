92c5a22fd38395d7eff9e5b26d7f724ea1b59c7d
Ricardo Rivas <richistron@gmail.com>
2022-09-15 20:53:26 -0700

    NumeroMasGrandeIlimitado agregado

--- PESemana3/MenuOpciones.cs
+++ PESemana3/MenuOpciones.cs
@@ -2,16 +2,19 @@
 
 public class MenuOpciones
 {
-    private bool valido = false;
+    private bool valido;
+    private bool terminado;
 
     public void Iniciar()
     {
-        while (!valido)
+        while (!valido && !terminado)
         {
             Console.WriteLine("Elige una opción:");
             Console.WriteLine("\t 1) Adivina el número");
             Console.WriteLine("\t 2) Encuentra el número más grande con sort");
             Console.WriteLine("\t 3) Encuentra el número más grande con if y else");
+            Console.WriteLine("\t 4) Encuentra el número más grande con recursion");
+            Console.WriteLine("\t c) Cancelar");
             Opciones();
         }
     }
@@ -19,8 +22,6 @@ public class MenuOpciones
     private void Opciones()
     {
         string? opt = Console.ReadLine();
-
-        // TODO, numero mas grande array dinámico 
 
         switch (opt)
         {
@@ -40,6 +41,16 @@ public class MenuOpciones
                 valido = true;
                 NumeroMasGrandeIfElse numeroMasGrandeIfElse = new NumeroMasGrandeIfElse();
                 numeroMasGrandeIfElse.ComparaNumeros();
+                break;
+
+            case "4":
+                valido = true;
+                NumeroMasGrandeIlimitado numeroMasGrandeIlimitado = new NumeroMasGrandeIlimitado();
+                numeroMasGrandeIlimitado.ComparaNumeros();
+                break;
+
+            case "c":
+                terminado = true;
                 break;
 
             default:
---
+++ PESemana3/NumeroMasGrandeIlimitado.cs
@@ -0,0 +1,58 @@
+﻿namespace PESemana3;
+
+public class NumeroMasGrandeIlimitado
+{
+    private int[] numeros;
+    private bool terminado;
+
+    public NumeroMasGrandeIlimitado()
+    {
+        int numero;
+        List<int> lista = new List<int>();
+
+        while (!terminado)
+        {
+            numero = LeeNumero();
+            if (!terminado)
+                lista.Add(numero);
+        }
+
+        numeros = lista.ToArray();
+    }
+
+    public void ComparaNumeros()
+    {
+        Array.Sort(numeros);
+        Array.Reverse(numeros);
+        Console.WriteLine("El número más grade es {0}", numeros[0]);
+    }
+
+    private int LeeNumero()
+    {
+        string? entrada;
+        bool valido = false;
+        int numero = 0;
+
+        while (!valido)
+        {
+            Console.WriteLine("Escribe un número o presiona 'c' para continuar");
+            entrada = Console.ReadLine();
+            if (entrada == "c")
+            {
+                terminado = true;
+                valido = true;
+            }
+
+            if (int.TryParse(entrada, out numero))
+            {
+                valido = true;
+            }
+            else
+            {
+                Console.WriteLine("El valor '{0}' es inválido", entrada);
+            }
+        }
+
+        return numero;
+    }
+}
